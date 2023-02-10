5529b2720ecccb88b7e7c11a78d87499e36de8f1
SeaMoon <seamoon.dev@gmail.com>
2023-02-10 20:45:56 +0900

    init project

---
+++ src/main/java/com/seamoon/pjt/springlec/SpringlecApplication.java
@@ -0,0 +1,13 @@
+package com.seamoon.pjt.springlec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringlecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seamoon/pjt/springlec/SpringlecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamoon.pjt.springlec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
